79703b8bcf89b8fe01d6f9665f76c7ec8548b462
racheallarimer <racheal.larimer@gmail.com>
2021-05-21 18:27:29 -0700

    fix sanity client

--- web/sanity.js
+++ web/sanity.js
@@ -1,16 +1,10 @@
 import { createClient, createImageUrlBuilder } from "next-sanity"
 
 const config = {
-  /**
-   * Find your project ID and dataset in `sanity.json` in your studio project.
-   * These are considered “public”, but you can use environment variables
-   * if you want differ between local dev and production.
-   *
-   * https://nextjs.org/docs/basic-features/environment-variables
-   **/
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV === "production",
+  dataset: "production",
+  projectId: n65ed0r6,
+  useCdn: true,
+  apiVersion: '2020-05-20'
   /**
    * Set useCdn to `false` if your application require the freshest possible
    * data always (potentially slightly slower and a bit more expensive).
